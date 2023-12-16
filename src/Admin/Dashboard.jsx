3f1bbae0b6e5cd96b6b797aa33c4f4eb682a2737
Huy <diepngochuy3@gmail.com>
2023-12-16 18:58:13 +0700

    update API URL

--- src/Admin/Dashboard.jsx
+++ src/Admin/Dashboard.jsx
@@ -108,7 +108,7 @@ function Dashboard() {
                                             (
                                                 <>
                                                     {loading ? (
-                                                        <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+                                                        <div className="d-flex justify-content-center align-items-center" >
                                                             <div className="spinner-border text-primary" role="status">
                                                                 <span className="visually-hidden">Loading...</span>
                                                             </div>
