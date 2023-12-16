2fde8cef780b5b6427f03aa4bdb204ddfd535260
Huy <diepngochuy3@gmail.com>
2023-12-16 18:30:39 +0700

    update API URL

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -1,15 +1,10 @@
-import PropTypes from 'prop-types';
 
 
-export default function Loading({ color = 'primary', text = 'Loading...' }) {
-    <div className={`d-flex justify-content-center align-items-center`} style={{ minHeight: '100vh' }}>
-        <div className={`spinner-border text-${color}`} role="status">
-            <span className="visually-hidden">{text}</span>
+export default function Loading() {
+    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
         </div>
     </div>
 }
 
-Loading.propTypes = {
-    color: PropTypes.string, // Màu sắc của spinner, mặc định là 'primary'
-    text: PropTypes.string,  // Văn bản hiển thị, mặc định là 'Loading...'
-};
