f60b22adaab855ccabff3a79905fdf827cbf33a4
Huy <diepngochuy3@gmail.com>
2023-12-18 14:01:42 +0700

    update API URL

--- src/Customer/CreatePlaylist.jsx
+++ src/Customer/CreatePlaylist.jsx
@@ -17,6 +17,8 @@ import SearchAppBar from '../component/SearchAppBar';
 function CreatePlaylist() {
     const navigate = useNavigate();
     const [activeStep, setActiveStep] = useState(0);
+    const [loading, setLoading] = useState(null);
+
     const [data, setData] = useState({
         collection_name: '',
         image: null,
@@ -70,10 +72,11 @@ function CreatePlaylist() {
         const formData = new FormData();
         formData.append('collection_name', data.collection_name);
         formData.append('image', data.image);
-
+        setLoading(true)
         axios.post(`${apiUrl}/createPlaylist/` + userId, formData)
             .then((res) => {
                 if (res.data.Status === 'Success') {
+                    setLoading(false)
                     navigate('/playlist/' + userId);
                 } else {
                     alert('Collection name is existed. Please try again!');
@@ -140,56 +143,66 @@ function CreatePlaylist() {
             <div className='d-flex flex-column align-items-center pt-4'>
                 <h3 className="d-flex justify-content-center" style={{ color: '#0d6efd', fontWeight: 'bold' }}>New Playlist</h3>
             </div>
-            <div className='d-flex flex-column align-items-center justify-content-center pt-5' style={{ minHeight: '60vh' }}>
-                <div className='step-container'>
-                    <Box sx={{ width: '100%' }}>
-                        <Stepper activeStep={activeStep}>
-                            {steps.map((label) => {
-                                return (
-                                    <Step key={label}>
-                                        <StepLabel>{label}</StepLabel>
-                                    </Step>
-                                );
-                            })}
-                        </Stepper>
-                        <div className='step-content'>
-                            {activeStep === steps.length ? (
-                                <React.Fragment>
-                                    <Typography variant="h6" sx={{ mt: 2, mb: 1 }}>
-                                        All steps completed - you&apos;re finished
-                                    </Typography>
-                                    <Box sx={{ display: 'flex', justifyContent: 'center', pt: 2 }}>
-                                        <Button variant="contained" onClick={handleClose} color="secondary">
-                                            Close
-                                        </Button>
-                                    </Box>
-                                </React.Fragment>
-                            ) : (
-                                <div>
-                                    {renderStepContent()}
-                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-                                        <Button
-                                            variant="contained"
-                                            disabled={activeStep === 0}
-                                            onClick={handleBack}
-                                            sx={{ mt: 1, mr: 2 }}
-                                        >
-                                            Back
-                                        </Button>
-                                        <Button
-                                            variant="contained"
-                                            onClick={activeStep === steps.length - 1 ? handleSubmit : handleNext}
-                                            sx={{ mt: 1, mr: 1 }}
-                                        >
-                                            {activeStep === steps.length - 1 ? 'Finish' : 'Continue'}
-                                        </Button>
-                                    </Box>
-                                </div>
-                            )}
-                        </div>
-                    </Box>
+            {loading ? (
+                <div className="d-flex flex-column justify-content-center align-items-center" style={{ paddingTop: '100px' }}>
+                    <div className="spinner-border text-primary" role="status">
+                        <p className="visually-hidden">Loading...</p>
+                    </div>
                 </div>
-            </div>
+            ) :
+                <>
+                    <div className='d-flex flex-column align-items-center justify-content-center pt-5' style={{ minHeight: '60vh' }}>
+                        <div className='step-container'>
+                            <Box sx={{ width: '100%' }}>
+                                <Stepper activeStep={activeStep}>
+                                    {steps.map((label) => {
+                                        return (
+                                            <Step key={label}>
+                                                <StepLabel>{label}</StepLabel>
+                                            </Step>
+                                        );
+                                    })}
+                                </Stepper>
+                                <div className='step-content'>
+                                    {activeStep === steps.length ? (
+                                        <React.Fragment>
+                                            <Typography variant="h6" sx={{ mt: 2, mb: 1 }}>
+                                                All steps completed - you&apos;re finished
+                                            </Typography>
+                                            <Box sx={{ display: 'flex', justifyContent: 'center', pt: 2 }}>
+                                                <Button variant="contained" onClick={handleClose} color="secondary">
+                                                    Close
+                                                </Button>
+                                            </Box>
+                                        </React.Fragment>
+                                    ) : (
+                                        <div>
+                                            {renderStepContent()}
+                                            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+                                                <Button
+                                                    variant="contained"
+                                                    disabled={activeStep === 0}
+                                                    onClick={handleBack}
+                                                    sx={{ mt: 1, mr: 2 }}
+                                                >
+                                                    Back
+                                                </Button>
+                                                <Button
+                                                    variant="contained"
+                                                    onClick={activeStep === steps.length - 1 ? handleSubmit : handleNext}
+                                                    sx={{ mt: 1, mr: 1 }}
+                                                >
+                                                    {activeStep === steps.length - 1 ? 'Finish' : 'Continue'}
+                                                </Button>
+                                            </Box>
+                                        </div>
+                                    )}
+                                </div>
+                            </Box>
+                        </div>
+                    </div>
+                </>
+            }
         </>
     );
 }
