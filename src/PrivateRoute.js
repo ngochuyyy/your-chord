91a0b7fd64bcff705e08a607ffda11890c744bdc
Huy <diepngochuy3@gmail.com>
2023-12-19 22:55:51 +0700

    update API URL

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,15 +1,16 @@
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { isAuthenticated } from './authService'; // Adjust the import according to your authService
 
 const PrivateRoute = ({ ...rest }) => {
+    const navigate = useNavigate();
     const token = sessionStorage.getItem('token');
     const userId = token ? token.split(':')[0] : null;
 
     if (!isAuthenticated() || (userId && userId !== rest.userId)) {
         // Redirect to login if not authenticated or userId is different
-        return <Navigate to="/login" />;
+        navigate('/login');
+        return null; // You can return null or any other content if needed
     }
-
 
 };
 
