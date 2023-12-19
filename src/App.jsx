ff1f392af89f68db11d9aed6c1c858d51e69deda
Huy <diepngochuy3@gmail.com>
2023-12-20 01:40:22 +0700

    update API URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 // import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import SignUp from './SignUp'
 import Login from './Login'
@@ -69,8 +71,130 @@ import RequestChordStatus from './Customer/RequestChordStatus';
 import ViewRequestChord from './Customer/ViewRequestChord';
 import ViewRequestChordMusician from './Musician/ViewRequestChordMusician';
 function App() {
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    const userId = token.split(':')[0];
+    const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+
+    axios.get(`${apiUrl}/getProfile/` + userId)
+      .then(res => {
+        if (res.data.Status === "Success") {
+          setUserRole(res.data.role); // assuming the role is available in the API response
+        } else {
+          alert("Error");
+        }
+      })
+      .catch(err => console.log(err));
+  }, []);
+  const renderAdminRoutes = () => {
+    if (userRole === 'admin') {
+      return (
+        <>
+          <Route path='/' element={<Dashboard />}>
+            <Route path='/profile/:userId' element={<Profile />}></Route>
+            <Route path='/song' element={<Song />}></Route>
+            <Route path='/manageAccount' element={<ManageAccount />}></Route>
+            <Route path='/requestAccount' element={<RequestAccount />}></Route>
+            <Route path='/manageFeedback/:userId' element={<ManageFeedback />}></Route>
+            <Route path='/viewFeedback/:id' element={<ViewFeedback />}></Route>
+            <Route path='/createSong' element={<CreateSong />}></Route>
+            <Route path='/requestCourse' element={<RequestListCourse />}></Route>
+            <Route path='/viewRequestCourse/:id/' element={<ViewRequestCourse />}></Route>
+          </Route>
+          <Route path='/viewSong/:id' element={<ViewSong />}></Route>
+          <Route path='/artistAdmin/:id/:artist_id' element={<ArtistAdmin />}></Route>
+        </>
+      );
+    }
+    return null;
+  };
+
+  const renderChordManagerRoutes = () => {
+    if (userRole === 'chord') {
+      return (
+        <>
+          <Route path='/' element={<DashboardChordManager />}>
+            <Route path='/verifySong' element={<VerifySong />}></Route>
+            <Route path='/profileChordManager/:userId' element={<ProfileChordManager />}></Route>
+            <Route path='/songChordManager' element={<SongChordManager />}></Route>
+          </Route>
+          <Route path='/viewSongChordManager/:id' element={<ViewSongChordManager />}></Route>
+          <Route path='/artistChordManager/:id/:artist_id' element={<ArtistChordManager />}></Route>
+        </>
+      );
+    }
+    return null;
+  };
+  const renderMusicianRoutes = () => {
+    if (userRole === 'musician') {
+      return (
+        <>
+          <Route path='/' element={<DashboardMusician />}>
+            <Route path='/chordMissMusician' element={<ChordMissMusician />}></Route>
+            <Route path='/editSongMusician/:id' element={<EditSongMusician />}></Route>
+            <Route path='/profileMusician/:userId' element={<ProfileMusician />}></Route>
+            <Route path='/chordMusician' element={<ChordMusician />}></Route>
+            <Route path='/manageBeat' element={<ManageBeat />}></Route>
+            <Route path='/orderMusician' element={<OrderMusician />}></Route>
+            <Route path='/createChord' element={<CreateChord />}></Route>
+            <Route path='/songMusician' element={<SongMusician />}></Route>
+            <Route path='/rejectSong' element={<RejectSong />}></Route>
+            <Route path='/viewOrderMusician/:id/' element={<ViewOrderMusician />}></Route>
+            <Route path='/manageCourse/:userId' element={<ManageCourse />}></Route>
+            <Route path='/rejectCourse/:userId' element={<RejectCourse />}></Route>
+            <Route path='/viewRejectCourse/:id' element={<ViewRejectCourse />}></Route>
+            <Route path='/requestChordMusician' element={<RequestChordMusician />}></Route>
+            <Route path='/viewRequestChordMusician/:id/' element={<ViewRequestChordMusician />}></Route>
+          </Route>
+          <Route path='/viewSongMusician/:id' element={<ViewSongMusician />}></Route>
+          <Route path='/songBeatManager/:user_id/:beat_type' element={<SongBeatManager />}></Route>
+          <Route path='/artistMusician/:id/:artist_id' element={<ArtistMusician />}></Route>
+
+        </>
+      );
+    }
+    return null;
+  };
+  const renderUsernRoutes = () => {
+    if (userRole === 'user') {
+      return (
+        <>
+          <Route path='/' element={<DashboardCustomer />}>
+            <Route path='/songCustomer/:userId' element={<SongCustomer />}></Route>
+            <Route path='/profileCustomer/:userId' element={<ProfileCustomer />}></Route>
+            <Route path='/createPlaylist/:userId' element={<CreatePlaylist />}></Route>
+            <Route path='/playlist/:userId' element={<Playlist />}></Route>
+            <Route path='/viewPlaylist/:id' element={<ViewPlaylist />}></Route>
+            <Route path='/feedback/:userId' element={<Feedback />}></Route>
+            <Route path='/viewFeedbackCustomer/:id' element={<ViewFeedbackCustomer />}></Route>
+            <Route path='/viewFeedbackCustomerAll/:id' element={<ViewFeedbackCustomerAll />}></Route>
+            <Route path='/transactionHistory/:userId' element={<TransactionHistory />}></Route>
+            <Route path='/beat/:user_id/' element={<Beat />}></Route>
+            <Route path='/order/:user_id/' element={<OrderCustomer />}></Route>
+            <Route path='/orderStatus/:user_id/' element={<OrderStatus />}></Route>
+            <Route path='/viewOrderCustomer/:id/' element={<ViewOrderCustomer />}></Route>
+            <Route path='/course' element={<Course />}></Route>
+            <Route path='/requestChord/:user_id/' element={<RequestChord />}></Route>
+            <Route path='/requestChordStatus/:user_id/' element={<RequestChordStatus />}></Route>
+            <Route path='/viewRequestChord/:id/' element={<ViewRequestChord />}></Route>
+            <Route path='/editRequestChordMusician/:id' element={<EditRequestChordMusician />}></Route>
+          </Route>
+          <Route path='/songBeat/:user_id/:beat_type' element={<SongBeat />}></Route>
+          <Route path='/chord' element={<Chord />}></Route>
+          <Route path='/searchChord' element={<SearchChord />}></Route>
+          <Route path='/viewSongCustomer/:id' element={<ViewSongCustomer />}></Route>
+          <Route path='/artist/:id/:artist_id' element={<Artist />}></Route>
+
+        </>
+      );
+    }
+    return null;
+  };
   return (
     // <BrowserRouter>
+
     <Router>
       <Routes>
         <Route
@@ -79,7 +203,15 @@ function App() {
         />
         <Route path='/login' element={<Login />}></Route>
         <Route path='/signUp' element={<SignUp />}></Route>
-        <Route path='/' element={<Dashboard />}>
+
+
+
+
+
+        {/* ADMIN ROLE */}
+        {renderAdminRoutes()}
+
+        {/* <Route path='/' element={<Dashboard />}>
           <Route path='/profile/:userId' element={<Profile />}></Route>
           <Route path='/song' element={<Song />}></Route>
           <Route path='/manageAccount' element={<ManageAccount />}></Route>
@@ -89,23 +221,30 @@ function App() {
           <Route path='/createSong' element={<CreateSong />}></Route>
           <Route path='/requestCourse' element={<RequestListCourse />}></Route>
           <Route path='/viewRequestCourse/:id/' element={<ViewRequestCourse />}></Route>
-
         </Route>
         <Route path='/viewSong/:id' element={<ViewSong />}></Route>
-        <Route path='/artistAdmin/:id/:artist_id' element={<ArtistAdmin />}></Route>
+        <Route path='/artistAdmin/:id/:artist_id' element={<ArtistAdmin />}></Route> */}
+
+
+
+
 
         {/* CHORD MANAGER ROLE */}
-        <Route path='/' element={<DashboardChordManager />}>
+        {renderChordManagerRoutes()}
+
+        {/* <Route path='/' element={<DashboardChordManager />}>
           <Route path='/verifySong' element={<VerifySong />}></Route>
           <Route path='/profileChordManager/:userId' element={<ProfileChordManager />}></Route>
           <Route path='/songChordManager' element={<SongChordManager />}></Route>
         </Route>
         <Route path='/viewSongChordManager/:id' element={<ViewSongChordManager />}></Route>
-        <Route path='/artistChordManager/:id/:artist_id' element={<ArtistChordManager />}></Route>
+        <Route path='/artistChordManager/:id/:artist_id' element={<ArtistChordManager />}></Route> */}
 
 
         {/* MUSICIAN ROLE */}
-        <Route path='/' element={<DashboardMusician />}>
+        {renderMusicianRoutes()}
+
+        {/* <Route path='/' element={<DashboardMusician />}>
           <Route path='/chordMissMusician' element={<ChordMissMusician />}></Route>
           <Route path='/editSongMusician/:id' element={<EditSongMusician />}></Route>
           <Route path='/profileMusician/:userId' element={<ProfileMusician />}></Route>
@@ -121,15 +260,16 @@ function App() {
           <Route path='/viewRejectCourse/:id' element={<ViewRejectCourse />}></Route>
           <Route path='/requestChordMusician' element={<RequestChordMusician />}></Route>
           <Route path='/viewRequestChordMusician/:id/' element={<ViewRequestChordMusician />}></Route>
-
         </Route>
         <Route path='/viewSongMusician/:id' element={<ViewSongMusician />}></Route>
         <Route path='/songBeatManager/:user_id/:beat_type' element={<SongBeatManager />}></Route>
-        <Route path='/artistMusician/:id/:artist_id' element={<ArtistMusician />}></Route>
+        <Route path='/artistMusician/:id/:artist_id' element={<ArtistMusician />}></Route> */}
 
 
         {/* CUSTOMER ROLE */}
-        <Route path='/' element={<DashboardCustomer />}>
+        {renderUsernRoutes()}
+
+        {/* <Route path='/' element={<DashboardCustomer />}>
           <Route path='/songCustomer/:userId' element={<SongCustomer />}></Route>
           <Route path='/profileCustomer/:userId' element={<ProfileCustomer />}></Route>
           <Route path='/createPlaylist/:userId' element={<CreatePlaylist />}></Route>
@@ -154,7 +294,7 @@ function App() {
         <Route path='/chord' element={<Chord />}></Route>
         <Route path='/searchChord' element={<SearchChord />}></Route>
         <Route path='/viewSongCustomer/:id' element={<ViewSongCustomer />}></Route>
-        <Route path='/artist/:id/:artist_id' element={<Artist />}></Route>
+        <Route path='/artist/:id/:artist_id' element={<Artist />}></Route> */}
 
       </Routes>
     </Router>
