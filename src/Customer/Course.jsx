396ec828c85a5788ab05c0e1400584a460c2e9f0
Huy <diepngochuy3@gmail.com>
2023-12-21 11:12:23 +0700

    update API URL

--- src/Customer/Course.jsx
+++ src/Customer/Course.jsx
@@ -114,10 +114,11 @@ function Course() {
                 </ThemeProvider>
             </Box>
             {loading ? (
-                <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+                <div className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
                     <div className="spinner-border text-primary" role="status">
                         <span className="visually-hidden">Loading...</span>
                     </div>
+                    <p>Loading...</p>
                 </div>
             )
                 :
