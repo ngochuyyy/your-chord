c3d7c894067051853e19c99308e733aa923b2489
Huy <diepngochuy3@gmail.com>
2023-12-26 16:41:57 +0700

    update API URL

--- src/Musician/OrderMusicianAccept.jsx
+++ src/Musician/OrderMusicianAccept.jsx
@@ -211,6 +211,7 @@ function OrderMusicianAccept() {
                     <div className="spinner-border text-primary" role="status">
                         <span className="visually-hidden">Loading...</span>
                     </div>
+                    <p>Loading...</p>
                 </div>
             )
                 :
