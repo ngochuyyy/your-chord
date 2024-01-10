2b83a69b0d9cf31aedab0aac92edd7cfefc3d4be
Huy <diepngochuy3@gmail.com>
2024-01-10 15:58:40 +0700

    update API URL

--- src/component/InfoContainer.jsx
+++ src/component/InfoContainer.jsx
@@ -1,6 +1,7 @@
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 function InfoContainer() {
     return (
@@ -9,6 +10,8 @@ function InfoContainer() {
                 <p className="your-chord-text">
                     <span className="animated-text">Your Chord @2023</span>
                     <br />
+                    <Link to="mailto:yourchord@example.com" style={{ textDecoration: 'none' }}>yourchord@example.com</Link> |
+                    <p>Hotline: 0918771818</p>
                 </p>
             </div>
         </>
