58c8050f4eb2e68e08ffc8981592f9da4c5dc5c3
Huy <diepngochuy3@gmail.com>
2024-01-11 02:02:09 +0700

    update API URL

--- src/Musician/SongBeatManager.jsx
+++ src/Musician/SongBeatManager.jsx
@@ -264,6 +264,7 @@ function SongBeatManager() {
                 <>
                     <div className="d-flex">
                         <div className="col-md-8" >
+                            <div>{`${beat_type}`}</div>
                             {data.length === 0 ? (
                                 <div style={{
                                     margin: '10px', marginTop: '80px', textAlign: 'center'
