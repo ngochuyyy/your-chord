7c633f17b8ebaa9672e1851dffc58915c3a92050
Huy <diepngochuy3@gmail.com>
2024-01-10 15:55:36 +0700

    update API URL

--- src/component/InfoContainer.jsx
+++ src/component/InfoContainer.jsx
@@ -1,12 +1,14 @@
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function InfoContainer() {
     return (
         <>
             <div className="project-info-container" style={{ width: '100%', textAlign: 'center' }}>
                 <p className="your-chord-text">
                     <span className="animated-text">Your Chord @2023</span>
+                    <br />
                 </p>
             </div>
         </>
