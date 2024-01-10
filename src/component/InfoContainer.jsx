3649809c0a04046105bca0f98a0e4393bf031730
Huy <diepngochuy3@gmail.com>
2024-01-10 16:00:53 +0700

    update API URL

--- src/component/InfoContainer.jsx
+++ src/component/InfoContainer.jsx
@@ -6,11 +6,21 @@ import { Link } from 'react-router-dom';
 function InfoContainer() {
     return (
         <>
-            <div className="project-info-container" style={{ width: '100%', textAlign: 'center' }}>
+            {/* <div className="project-info-container" style={{ width: '100%', textAlign: 'center' }}>
                 <p className="your-chord-text">
                     <span className="animated-text">Your Chord @2023</span>
                     <br />
-                    <Link to="mailto:yourchord@example.com" style={{ textDecoration: 'none' }}>yourchord@example.com</Link> |
+                    <Link to="/introduction" style={{ textDecoration: 'none' }}> Introduction</Link> |
+                    <Link to="/terms" style={{ textDecoration: 'none' }}> Terms of Service</Link> |
+                    <Link to="/copyright" style={{ textDecoration: 'none' }}> Copyright Policy</Link> |
+                    <Link to="/guide" style={{ textDecoration: 'none' }}> How to Guide</Link>
+                </p>
+            </div> */}
+            <div className="project-info-container" style={{ width: '100%', textAlign: 'center' }}>
+                <p className="your-chord-text">
+                    <span className="animated-text">YourChord @2023</span>
+                    <br />
+                    <Link to="mailto:diepngochuy3@gmail.com" style={{ textDecoration: 'none' }}>diepngochuy3@gmail.com</Link> |
                     <p>Hotline: 0918771818</p>
                 </p>
             </div>
