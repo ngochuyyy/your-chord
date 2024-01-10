1f5d75c4587c7b9afeb053368a6c167f1d2160f5
Huy <diepngochuy3@gmail.com>
2024-01-10 16:06:22 +0700

    update API URL

--- src/component/InfoContainer.jsx
+++ src/component/InfoContainer.jsx
@@ -21,7 +21,7 @@ function InfoContainer() {
                     <span className="animated-text">YourChords @2023</span>
                     <br />
                     <Link to="mailto:diepngochuy3@gmail.com" style={{ textDecoration: 'none' }}>diepngochuy3@gmail.com</Link> |
-                    <Link style={{ textDecoration: 'none' }}> Hotline: <p>0918771818</p></Link>
+                    <Link style={{ textDecoration: 'none' }}> Hotline: <span>0918771818</span></Link>
                 </p>
             </div>
         </>
