cf2c9c4cd92d01b0f3f8c6ab863990ddb55fb749
Huy <diepngochuy3@gmail.com>
2024-01-15 13:17:16 +0700

    update API URL

--- src/Customer/CoursePage.jsx
+++ src/Customer/CoursePage.jsx
@@ -141,7 +141,7 @@ function CoursePage() {
                                     transition: 'background-color 0.3s, color 0.3s',
                                 },
                                 borderRadius: '40px',
-                                height: '35px',
+                                height: '50px',
                                 padding: '12px',
                             }}>
                             {tabVisible ? <ChevronLeftIcon style={{ fontSize: 32 }} /> : <ChevronRightIcon style={{ fontSize: 32 }} />}
