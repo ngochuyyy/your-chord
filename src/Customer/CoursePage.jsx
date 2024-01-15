945871aa895805e5448242525a052b9f8732b542
Huy <diepngochuy3@gmail.com>
2024-01-15 14:26:09 +0700

    update API URL

--- src/Customer/CoursePage.jsx
+++ src/Customer/CoursePage.jsx
@@ -165,7 +165,9 @@ function CoursePage() {
                             onClick={() => setTabVisible(!tabVisible)}
                             style={{
                                 height: '48px',
-                                border: 1,
+                                borderRight: 1,
+                                borderTop: 1,
+                                borderBottom: 1,
                                 borderColor: tabVisible ? '' : "#0d6efd",
                                 borderTopRightRadius: '4px',
                                 borderBottomRightRadius: '4px',
