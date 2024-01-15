2fa0e51c382427cc51b0c9f0ca5f3f71b4554082
Huy <diepngochuy3@gmail.com>
2024-01-15 14:33:52 +0700

    update API URL

--- src/Customer/CoursePage.jsx
+++ src/Customer/CoursePage.jsx
@@ -168,7 +168,7 @@ function CoursePage() {
                                 borderRight: 1,
                                 borderTop: 1,
                                 borderBottom: 1,
-                                borderLeft: tabVisible ? "" : 1,
+                                borderLeft: tabVisible ? 0 : 1,
                                 borderColor: tabVisible ? "" : "#0d6efd",
                                 borderTopRightRadius: '4px',
                                 borderBottomRightRadius: '4px',
