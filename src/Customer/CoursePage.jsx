52c1e4c563298d48635b4985598c24f024425469
Huy <diepngochuy3@gmail.com>
2024-01-15 13:55:50 +0700

    update API URL

--- src/Customer/CoursePage.jsx
+++ src/Customer/CoursePage.jsx
@@ -166,7 +166,9 @@ function CoursePage() {
                             onClick={() => setTabVisible(!tabVisible)}
                             sx={{
                                 height: '40px',
-                                border: '1px solid #1A73E8',
+                                borderTop: '1px solid #1A73E8',
+                                borderRight: '1px solid #1A73E8',
+                                borderBottom: '1px solid #1A73E8',
                                 borderRadius: '4px',
                                 '&:hover': {
                                     border: '1px solid #1565C0',
