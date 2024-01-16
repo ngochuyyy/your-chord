7b94de9acb6bbe3cd75333ddfb2030d65c653153
Huy <diepngochuy3@gmail.com>
2024-01-16 18:32:27 +0700

    update API URL

--- src/Customer/CoursePage.jsx
+++ src/Customer/CoursePage.jsx
@@ -149,7 +149,7 @@ function CoursePage() {
                                         borderBottom: 1,
                                         borderBottomRightRadius: '20px',
                                         borderColor: 'divider',
-                                        width: '22%',
+                                        width: '100%',
                                         height: '70vh',
                                     }}>
                                     {filteredRequestCourse.map((course, index) => (
