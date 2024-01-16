b5d0f19b6ccca650d6342e77cf3f0cc02db9fb06
Huy <diepngochuy3@gmail.com>
2024-01-16 18:25:17 +0700

    update API URL

--- src/Customer/CoursePage.jsx
+++ src/Customer/CoursePage.jsx
@@ -136,7 +136,7 @@ function CoursePage() {
                     }}>
 
                         {tabVisible && (
-                            <Scrollbars style={{ width: '22%', height: '65vh' }}>
+                            <Scrollbars style={{ width: '24%', height: '65vh' }}>
 
                                 <Tabs
                                     orientation="vertical"
