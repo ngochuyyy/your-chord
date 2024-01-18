954c108c1b1edb7866c0e8794b276090e981dbcd
Huy <diepngochuy3@gmail.com>
2024-01-18 15:32:04 +0700

    update API URL

--- src/Admin/RequestAccountPage.jsx
+++ src/Admin/RequestAccountPage.jsx
@@ -403,7 +403,16 @@ function RequestAccountPage() {
                                                                 style={{ width: '100px', textAlign: 'center', backgroundColor: '#28a745', color: '#fff', borderRadius: '40px' }}
                                                                 onClick={(e) => {
                                                                     e.stopPropagation();
-                                                                    handleAcceptAccountChordValidator(userAccount.username);
+                                                                    ModalConfirm.confirm({
+                                                                        title: 'Confirm',
+                                                                        content: `Are you sure you want to accept "${userAccount.username}" account - Chord Validator role ?`,
+                                                                        onOk() {
+                                                                            handleAcceptAccountChordValidator(userAccount.username);
+                                                                        },
+                                                                        onCancel() {
+                                                                            console.log('Cancel');
+                                                                        },
+                                                                    });
                                                                 }}>
                                                                 Accept
                                                             </Button>
@@ -411,7 +420,16 @@ function RequestAccountPage() {
                                                                 style={{ width: '100px', textAlign: 'center', backgroundColor: '#dc3545', color: '#fff', marginLeft: '5px', borderRadius: '40px' }}
                                                                 onClick={(e) => {
                                                                     e.stopPropagation();
-                                                                    handleRejectAccountChordValidator(userAccount.username);
+                                                                    ModalConfirm.confirm({
+                                                                        title: 'Confirm',
+                                                                        content: `Are you sure you want to reject "${userAccount.username}" account - Chord Validator role ?`,
+                                                                        onOk() {
+                                                                            handleRejectAccountChordValidator(userAccount.username);
+                                                                        },
+                                                                        onCancel() {
+                                                                            console.log('Cancel');
+                                                                        },
+                                                                    });
                                                                 }}>
                                                                 Decline
                                                             </Button>
@@ -550,7 +568,7 @@ function RequestAccountPage() {
                                                                     e.stopPropagation();
                                                                     ModalConfirm.confirm({
                                                                         title: 'Confirm',
-                                                                        content: `Are you sure you want to remove "${userAccount.username}" account ?`,
+                                                                        content: `Are you sure you want to accept "${userAccount.username}" account - Musician role ?`,
                                                                         onOk() {
                                                                             handleAcceptAccountMusician(userAccount.username);
                                                                         },
@@ -567,7 +585,7 @@ function RequestAccountPage() {
                                                                     e.stopPropagation();
                                                                     ModalConfirm.confirm({
                                                                         title: 'Confirm',
-                                                                        content: `Are you sure you want to remove "${userAccount.username}" account ?`,
+                                                                        content: `Are you sure you want to reject "${userAccount.username}" account - Musician role?`,
                                                                         onOk() {
                                                                             handleRejectAccountMusician(userAccount.username);
                                                                         },
