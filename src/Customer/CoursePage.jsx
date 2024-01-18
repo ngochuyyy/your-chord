8c5e721e58735ab80663ddfabbb9522828e420e7
Huy <diepngochuy3@gmail.com>
2024-01-18 14:25:53 +0700

    update API URL

--- src/Customer/CoursePage.jsx
+++ src/Customer/CoursePage.jsx
@@ -14,7 +14,6 @@ import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import moment from 'moment';
-import Scrollbars from 'react-custom-scrollbars';
 
 const darkTheme = createTheme({
     palette: {
@@ -136,35 +135,34 @@ function CoursePage() {
                     }}>
 
                         {tabVisible && (
-                            <Scrollbars style={{ width: '24%', height: 'auto' }}>
-                                <Tabs
-                                    orientation="vertical"
-                                    value={selectedCourse}
-                                    onChange={handleTabChange}
-                                    sx={{
-                                        position: 'flex',
-                                        borderRight: '1.5px solid',
-                                        borderTop: '1.5px solid',
-                                        borderBottom: '1.5px solid',
-                                        borderBottomRightRadius: '20px',
-                                        borderColor: 'divider',
-                                        width: '99%',
-                                        height: '73vh',
-                                    }}>
-                                    {filteredRequestCourse.map((course, index) => (
-                                        <Tab
-                                            key={index}
-                                            label={
-                                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                                    {selectedCourse === index && <PlayCircleIcon style={{ marginRight: '8px' }} />}
-                                                    <b>{course.course_name}</b>
-                                                </div>
-                                            }
-                                            style={{ borderTopRightRadius: '20px', borderBottomRightRadius: '20px' }}
-                                        />
-                                    ))}
-                                </Tabs>
-                            </Scrollbars>
+
+                            <Tabs
+                                orientation="vertical"
+                                value={selectedCourse}
+                                onChange={handleTabChange}
+                                sx={{
+                                    position: 'flex',
+                                    borderRight: '1.5px solid',
+                                    borderTop: '1.5px solid',
+                                    borderBottom: '1.5px solid',
+                                    borderBottomRightRadius: '20px',
+                                    borderColor: 'divider',
+                                    width: '22%',
+                                    height: '73vh',
+                                }}>
+                                {filteredRequestCourse.map((course, index) => (
+                                    <Tab
+                                        key={index}
+                                        label={
+                                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                                {selectedCourse === index && <PlayCircleIcon style={{ marginRight: '8px' }} />}
+                                                <b>{course.course_name}</b>
+                                            </div>
+                                        }
+                                        style={{ borderTopRightRadius: '20px', borderBottomRightRadius: '20px' }}
+                                    />
+                                ))}
+                            </Tabs>
                         )}
                         <button
                             onClick={() => setTabVisible(!tabVisible)}
