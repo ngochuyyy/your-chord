c7ea7a85b976f984127379c50e70abb34e77b670
Huy <diepngochuy3@gmail.com>
2024-01-20 14:34:27 +0700

    update API URL

--- src/Customer/OrderStatusPage.jsx
+++ src/Customer/OrderStatusPage.jsx
@@ -44,7 +44,6 @@ function OrderStatusPage() {
             title: 'Genre',
             dataIndex: 'genre',
             render: (text) => {
-                // Chuyển đổi text để chữ cái đầu tiên viết hoa
                 const genre = text.charAt(0).toUpperCase() + text.slice(1);
                 return genre;
             },
