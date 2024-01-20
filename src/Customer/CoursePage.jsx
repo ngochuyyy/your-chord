c5c90b0a30be43c1bcff24d8de0c04abb260b231
Huy <diepngochuy3@gmail.com>
2024-01-20 17:16:04 +0700

    update API URL

--- src/Customer/CoursePage.jsx
+++ src/Customer/CoursePage.jsx
@@ -136,7 +136,7 @@ function CoursePage() {
                     }}>
 
                         {tabVisible && (
-                            <Scrollbars style={{ width: '24%', height: '70vh' }}
+                            <Scrollbars style={{ width: '24%', height: filteredRequestCourse.length > 0 ? '70vh' : 0 }}
                                 renderThumbVertical={(props) => (
                                     <div
                                         {...props}
@@ -162,7 +162,6 @@ function CoursePage() {
                                         borderBottom: '1.5px solid',
                                         borderBottomRightRadius: '20px',
                                         borderColor: 'divider',
-                                        height: '100%',
                                         width: '100%',
                                     }}>
                                     {filteredRequestCourse.map((course, index) => (
